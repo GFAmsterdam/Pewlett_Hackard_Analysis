28e30e6b6410ff800ddcbc4319a8cd7c5f9c26cb
GFAmsterdam <kaitlinkennedy1112@gmail.com>
2022-08-02 22:31:26 -0400

    Updating queries and schema and adding retirement info csv

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -41,7 +41,6 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_emp (
@@ -50,7 +49,8 @@ CREATE TABLE dept_emp (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-PRIMARY KEY (emp_no)
+PRIMARY KEY (emp_no, dept_no)
 );
 
 SELECT * FROM departments;
+
